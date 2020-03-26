faedffec2e51615d8eeb300fa48cde45e5edcff0
veizour <veizour@users.noreply.github.com>
2020-03-26 17:45:39 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -2,7 +2,7 @@
 
 # Check if config exists. If not, copy in the sample config
 if [ -f /config/proxy-config.conf ]; then
-  echo "Using existing config file."
+  echo "Using saved config file."
 else
   echo "Creating config from template."
   mv /etc/apache2/000-default.conf /config/proxy-config.conf
@@ -19,4 +19,20 @@ else
   cp /root/crons.conf /config/crons.conf
 #  crontab /config/crons.conf
 #  cron
+fi
+
+# Check for OSSN DB config and add
+if [ -f /config/ossn.config.db ]; then
+  echo "Using saved OSSN DB config file."
+else
+  echo "Creating OSSN DB config from template."
+  mv /etc/apache2/ossn.config.db /config/ossn.config.db
+fi
+
+# Check for OSSN Site config and add
+if [ -f /config/ossn.config.site ]; then
+  echo "Using saved OSSN Site config file."
+else
+  echo "Creating OSSN Site config from template."
+  mv /etc/apache2/ossn.config.site /config/ossn.config.site
 fi
